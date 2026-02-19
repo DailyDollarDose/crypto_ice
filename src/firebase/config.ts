d937e45632511810f2b2cc59be14259d92a0b82e
itsy Shoot <shoaibak47.pk@gmail.com>
2026-02-19 06:31:21 +0000

    use these environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1878606508-1b70e",
-  "appId": "1:539128541110:web:f4708f335dd915b96d9b17",
-  "apiKey": "AIzaSyB7QHopH0NsuIKPM17bU2FTbWlpgRXZjlk",
-  "authDomain": "studio-1878606508-1b70e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "539128541110"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-project-id",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-app-id",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-api-key",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "your-project-id.firebaseapp.com",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-XXXXXXXXXX",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "your-sender-id",
 };
